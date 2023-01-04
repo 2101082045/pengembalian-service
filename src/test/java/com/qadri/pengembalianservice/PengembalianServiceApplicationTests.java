fdb28287d62e0dccc8adabfdecdbc9e9093ceff8
Rahmatul Qadri <113502622+2101082045@users.noreply.github.com>
2023-01-05 00:39:53 +0700

    Add files via upload

---
+++ src/test/java/com/qadri/pengembalianservice/PengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qadri.pengembalianservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
